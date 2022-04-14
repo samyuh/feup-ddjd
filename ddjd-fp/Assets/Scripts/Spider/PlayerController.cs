51391489e6069d5411af63b761108e8d8d6041c3
up201806490 <up201806490@fe.up.pt>
2022-04-15 00:40:55 +0100

    spider now follows

--- ddjd-fp/Assets/Scripts/Spider/PlayerController.cs
+++ ddjd-fp/Assets/Scripts/Spider/PlayerController.cs
@@ -10,20 +10,39 @@ public class PlayerController : MonoBehaviour
     private float MoveSpeed { get; } = 3.8f;
     private float RotSpeed { get; } = 80.0f;
 
+    private float distance;
+    private float minDistance = 5f;
+
     private GameObject player;
 
-   
+    void Start(){
+
+        player = GameObject.Find("Player");
+        
+
+    }
 
     void Update()
     {
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, player.transform.position - transform.position, out hit)){
+
+            distance = hit.distance;
+
+            if(distance >= minDistance){
+                transform.position = Vector3.MoveTowards(transform.position,player.transform.position,0.03f);
+            }
+
+        }
         // Handle keyboard control
         // This loop competes with AdjustBodyTransform() in LegController script to properly postion the body transform
 
         // float ws = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
-        transform.Translate(0, 0, 0.01f);
+        // transform.Translate(0, 0, 0.01f);
 
         // float ad = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
-        transform.Translate(0.01f, 0, 0);
+        // transform.Translate(0.01f, 0, 0);
 
         // if (Input.GetKey(KeyCode.Q))
         // {
