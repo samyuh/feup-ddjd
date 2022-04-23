f919e0aae423922f97e3c22625dd8abf2244e1d3
Pinto28 <Pinto28@github.com>
2022-04-23 18:12:55 +0100

    2 portals working almost perfectly

---
+++ ddjd-fp/Assets/Scripts/Camera/PortalCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerCameraOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerCameraOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
---
+++ ddjd-fp/Assets/Scripts/Portal/PortalTextureSetup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera cameraA;
+    public Material cameraMatA;
+
+    public Camera cameraB;
+    public Material cameraMatB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cameraA.targetTexture != null){
+            cameraA.targetTexture.Release();
+        }
+
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+
+        if(cameraB.targetTexture != null){
+            cameraB.targetTexture.Release();
+        }
+
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+    }
+}
