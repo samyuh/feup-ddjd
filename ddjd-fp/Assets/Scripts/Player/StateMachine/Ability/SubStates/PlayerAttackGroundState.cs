b768dc5931b4d6ea9e3a243d533141d44d9432a9
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-04-25 17:17:52 +0100

    Close up when player going to hit an enemy

--- ddjd-fp/Assets/Scripts/Player/StateMachine/Ability/SubStates/PlayerAttackGroundState.cs
+++ ddjd-fp/Assets/Scripts/Player/StateMachine/Ability/SubStates/PlayerAttackGroundState.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PlayerAttackGroundState : PlayerAbilityState {
     public float _elapsedTime;
+    private bool _dealDamage;
+    private bool _closeUp;
+
     public PlayerAttackGroundState(Player currentContext, StateMachine playerStateFactory, StateFactory stateFactory) : 
     base (currentContext, playerStateFactory, stateFactory) {}
 
     public override void EnterState() {
         base.EnterState();
+
+        _closeUp = true;
+        _dealDamage = true;
         _elapsedTime = 0f;
+        _context.Controller.enabled = false;
         _context.Animator.SetBool("Attack", true);
-        DealDamage();
+        
     }
 
     public override void ExitState() {
         base.ExitState();
         
+        _context.Controller.enabled = true;
         _context.Animator.SetBool("Attack", false);
     }
 
@@ -24,20 +32,42 @@ public class PlayerAttackGroundState : PlayerAbilityState {
         base.LogicUpdate();
 
         _elapsedTime += Time.deltaTime;
-        if(_elapsedTime > 0.5f) {
+        if (_elapsedTime > 0.5f) {
+            if (_dealDamage) DealDamage();
+
             if (_context.Animator.GetCurrentAnimatorStateInfo(0).length < _context.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + 0.5f) {
                 _stateMachine.ChangeState(_factory.IdleState);
             }
+        } else if (_elapsedTime > 0.25f) {
+            if (_closeUp) CloseUp();
+        }
+    }
+
+    private void CloseUp() {
+        _closeUp = false;
+        Vector3 spherePosition = new Vector3(_context.transform.position.x + 2f * _context.transform.TransformDirection(Vector3.forward).x, _context.transform.position.y + 0.3f, 
+                                        _context.transform.position.z + 2f * _context.transform.TransformDirection(Vector3.forward).z);
+        Collider[] hitColliders = Physics.OverlapSphere(spherePosition, 1.3f);
+
+        foreach (var hitCollider in hitColliders) {
+            if (hitCollider.gameObject.tag == "Enemy") {
+
+                Vector3 newPos = new Vector3(hitCollider.gameObject.transform.position.x - 1f, _context.Controller.transform.position.y, hitCollider.gameObject.transform.position.z);
+                _context.Controller.transform.position = newPos;
+            };
         }
     }
 
     private void DealDamage() {
-        Vector3 spherePosition = new Vector3(_context.transform.position.x + 1.616f * _context.transform.TransformDirection(Vector3.forward).x, 0.515f, 
-                                        _context.transform.position.z + 1.616f * _context.transform.TransformDirection(Vector3.forward).z);
+        _dealDamage = false;
+        Vector3 spherePosition = new Vector3(_context.transform.position.x + 0.616f * _context.transform.TransformDirection(Vector3.forward).x, _context.transform.position.y + 0.3f, 
+                                        _context.transform.position.z + 0.616f * _context.transform.TransformDirection(Vector3.forward).z);
 
-        Collider[] hitColliders = Physics.OverlapSphere(spherePosition, 0.9f);
+        Collider[] hitColliders = Physics.OverlapSphere(spherePosition, 0.2f);
         foreach (var hitCollider in hitColliders) {
-            Debug.Log(hitCollider.gameObject.name);
+            if (hitCollider.gameObject.tag == "Enemy") {
+                hitCollider.gameObject.SendMessage("ApplyDamage", 30);
+            };
         }
     }
 }
