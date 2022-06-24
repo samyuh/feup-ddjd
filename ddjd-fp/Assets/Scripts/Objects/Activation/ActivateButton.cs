a35f31cfb3c4f1d34af92e04dba7d64b7c51bdd3
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-06-24 21:12:22 +0100

    isle 2 coloors

--- ddjd-fp/Assets/Scripts/Objects/Activation/ActivateButton.cs
+++ ddjd-fp/Assets/Scripts/Objects/Activation/ActivateButton.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActivateButton : MonoBehaviour {
+    public enum ElementType { Neutral, Fire, Air }
+    public ElementType element;
+    
+    private string _strElement; 
+
+
     [SerializeField] private ColorManager _colors;
     [SerializeField] private Material _switchCrystal;
-    [SerializeField] private GameObject _button;
+    [SerializeField] private GameObject _armature;
     [SerializeField] private GameObject _collider;
 
     public int portal;
@@ -16,6 +22,14 @@ public class ActivateButton : MonoBehaviour {
     private float _elapsedTime = 0f;
 
     void Awake() {
+        if (element == ElementType.Fire) {
+            _strElement = "fire";
+        } else if (element == ElementType.Air) {
+            _strElement = "air";
+        } else {
+            _strElement = "neutral";
+        }
+
         _switchCrystal.SetColor("_Base_color",_colors.getColor("deactivated_crystal_base"));
         _switchCrystal.SetColor("_Top_color", _colors.getColor("deactivated_crystal_base"));
         _switchCrystal.SetColor("_Bottom_color", _colors.getColor("deactivated_crystal_base"));
@@ -24,40 +38,25 @@ public class ActivateButton : MonoBehaviour {
     void OnTriggerEnter(Collider collider) {
         if (collider.tag == "Player" && !_alreadyActive) {
             Events.OnActivatePortal.Invoke(portal);
-             _startPosition = _button.transform.position;
-            _targetPosition =  new Vector3(_button.transform.position.x, _button.transform.position.y - 0.15f, _button.transform.position.z);
+             _startPosition = _armature.transform.position;
+            _targetPosition =  new Vector3(_armature.transform.position.x, _armature.transform.position.y - 0.15f, _armature.transform.position.z);
             _distance = Vector3.Distance(_startPosition, _targetPosition);
              _alreadyActive = true;
-
-            //ColorSwatch result = _colors.ColorList.Find(x => x.Name == "neutral_base_color");
-            //Debug.Log(_colors.getColor("neutral_base_color"));
-
-
-
-            /*
-            deactivated_crystal_base > neutral_base_color
-            Top color  _Top_color
-            deactivated_crystal_top > neutral_top_color
-            Bottom color  _Bottom_color
-            deactivated_crystal_bottom > neutral_bottom_color
-            */
-
         }
     }
 
     void Update() {
         if (_alreadyActive) {
-            if (_button.transform.position != _targetPosition) {
-                // Distance moved equals elapsed time times speed
+            if (_armature.transform.position != _targetPosition) {
                 _elapsedTime += Time.deltaTime;
                 float distanceDisplacement = _elapsedTime * 0.15f;
                 float interpolationRatio = distanceDisplacement / _distance;
-                _button.transform.position = Vector3.Lerp(_startPosition, _targetPosition, interpolationRatio);
-                _collider.transform.position = _button.transform.position;
+                _armature.transform.position = Vector3.Lerp(_startPosition, _targetPosition, interpolationRatio);
+                _collider.transform.position = _armature.transform.position;
             } else {
-                _switchCrystal.SetColor("_Base_color",_colors.getColor("neutral_base_color"));
-                _switchCrystal.SetColor("_Top_color", _colors.getColor("neutral_top_color"));
-                _switchCrystal.SetColor("_Bottom_color", _colors.getColor("neutral_bottom_color"));
+                _switchCrystal.SetColor("_Base_color",_colors.getColor(_strElement + "_base_color"));
+                _switchCrystal.SetColor("_Top_color", _colors.getColor(_strElement + "_top_color"));
+                _switchCrystal.SetColor("_Bottom_color", _colors.getColor(_strElement + "_bottom_color"));
             } 
         }
     }
