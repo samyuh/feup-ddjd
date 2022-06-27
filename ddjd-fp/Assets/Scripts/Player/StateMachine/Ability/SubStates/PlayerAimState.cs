96ce8ea8ca78d318c39717a26acd252031f6e3f6
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-06-27 14:41:48 +0100

    Companion on top of player

--- ddjd-fp/Assets/Scripts/Player/StateMachine/Ability/SubStates/PlayerAimState.cs
+++ ddjd-fp/Assets/Scripts/Player/StateMachine/Ability/SubStates/PlayerAimState.cs
@@ -24,8 +24,6 @@ public class PlayerAimState : PlayerAbilityState {
         Events.OnToggleAim.Invoke();
         _context.PlayerInput.PlayerMeleeAttack.performed += OnThrow;
         _context.PlayerInput.PlayerAim.canceled += OnAimCancelled;
-
-        
     }  
 
     public override void ExitState() {
