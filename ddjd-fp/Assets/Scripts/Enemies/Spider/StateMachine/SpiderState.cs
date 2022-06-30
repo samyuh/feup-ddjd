06445ce6ae78ecd15c742ee0c08172decfd46af1
up201806490 <up201806490@fe.up.pt>
2022-06-30 16:36:38 +0100

    fixed speed again

--- ddjd-fp/Assets/Scripts/Enemies/Spider/StateMachine/SpiderState.cs
+++ ddjd-fp/Assets/Scripts/Enemies/Spider/StateMachine/SpiderState.cs
@@ -13,8 +13,8 @@ public abstract class SpiderState {
     protected float maxDistance = 50f;
     protected float followDistance = 2f;
     protected float speed = 0f;
-    protected float maxSpeed = 1.0f;
-    protected float acceleration = 0.1f;
+    protected float maxSpeed = 0.25f;
+    protected float acceleration = 0.2f;
     protected float deceleration = 1f;
 
     public SpiderState(SpiderController context, SpiderStateMachine stateMachine,  SpiderFactory stateFactory) { //GameObject target, GameObject context) {
